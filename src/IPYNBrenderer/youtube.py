f52886f85b73faa478d233ab1fb2560a8723f0a6
vedjangid19 <vedprakashjangid.jpr@gmail.com>
2022-10-30 14:03:03 +0530

    updated files for test

--- src/IPYNBrenderer/youtube.py
+++ src/IPYNBrenderer/youtube.py
@@ -15,9 +15,11 @@ def get_time_info(URL: str) -> int:
             )
 
     try:
-        split_val = URL.split('=')
+        split_val = URL.split("=")
+        if len(split_val) > 3:
+            raise InvalidURLException
         if "watch" in URL:
-            if ("&t" in URL):
+            if "&t" in URL:
                 vid_id, time = split_val[-2][:-2], int(split_val[-1][:-1])
                 _verify_vid_id_len(vid_id)
                 logger.info(f"video starts at: {time}")
@@ -53,18 +55,20 @@ def render_YouTube_video(URL: str, width: int = 780, height: int = 600) -> str:
             vid_ID = data["id"]
             embed_URL = f"https://www.youtube.com/embed/{vid_ID}?start={time}"
             logger.info(f"embed URL: {embed_URL}")
-            iframe = f"""<iframe 
-            width="{width}" height="{height}" 
-            src="{embed_URL}" 
-            title="YouTube video player" 
-            frameborder="0" 
-            allow="accelerometer; 
-            autoplay; clipboard-write; 
-            encrypted-media; gyroscope; 
+            iframe = f"""<iframe
+            width="{width}" height="{height}"
+            src="{embed_URL}"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer;
+            autoplay; clipboard-write;
+            encrypted-media; gyroscope;
             picture-in-picture" allowfullscreen>
             </iframe>
             """
             display.display(display.HTML(iframe))
             return "success"
+        else:
+            raise InvalidURLException
     except Exception as e:
         raise e
